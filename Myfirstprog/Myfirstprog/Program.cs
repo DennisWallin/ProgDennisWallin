1d67118e5ef5ebd8dc1881bba9e8cb547b3f9bb4
DennisWallin <31400210+DennisWallin@users.noreply.github.com>
2017-09-01 09:41:38 +0200

    Skapade mitt första program
    Skapade ett program där man skrev namn ålder osv.

---
+++ Myfirstprog/Myfirstprog/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Myfirstprog
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Skriv ditt namn:");
+
+            string namn;
+
+            namn = Console.ReadLine();
+
+            Console.WriteLine("Skriv nu in din ålder: ");
+
+            String ålder;
+
+            ålder = Console.ReadLine();
+
+            Console.WriteLine("Skriv in din adress:");
+
+            string adress;
+
+            adress = Console.ReadLine();
+           
+            Console.WriteLine("Har du några fritidsintressen? Nämn gärna några:");
+
+            String fritidsintressen;
+
+            fritidsintressen = Console.ReadLine();
+
+            Console.WriteLine("Hej " + namn + " och välkommen till mitt första program. Din adress är alltså: \n"
+                              + adress + ". Vad roligt att du också tycker om " + fritidsintressen + 
+                              ". Det passar \nverkligen dig som är " + ålder + ".");
+
+		}
+    }
+}
